14bf53ca683d27efde9330ec74e8108f748722ad
Erdi Sinan <133010975+erddisinan@users.noreply.github.com>
2023-12-17 20:10:46 +0300

    Player.java

--- src/Player.java
+++ src/Player.java
@@ -27,6 +27,37 @@ public class Player {
     public void setBoardCardIndex(int boardCardIndex){
         this.boardCardIndex=boardCardIndex;
     }
+    public void play(){
 
-    
+        if(isHuman){
+            boolean willPlay=true;
+            boolean end;
+            boolean stand;
+
+
+
+
+
+            if(willPlay){
+                System.out.println("This is your board");
+                for(int i = 0;i<boardCardIndex;i++){
+                    System.out.print((i+1)+" "+boardCards[i]+"|");
+                }
+                System.out.println();
+                System.out.println("Which cards do you want to play?");
+                for(int i = 0; i<currentCards.length;i++){
+                    if(currentCards[i]!=null){
+                        System.out.print((i+1)+" "+currentCards[i]+"|");
+                    }
+                }
+                int choice = sc.nextInt();
+                boardCards[boardCardIndex++]=currentCards[choice-1];
+                currentCards[choice-1]=null;
+                System.out.println();
+                boardPoints+= boardCards[boardCardIndex-1].getNumber();
+
+            }
+
+        }
+    }
 }
